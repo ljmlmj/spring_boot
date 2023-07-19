582d9e512c80e7385d4e8950cb6c0c4b8f8e63a4
ejm8467 <ejm8467@gmail.com>
2023-07-19 19:14:01 +0900

    .

---
+++ chap65_mybatis_restapi/src/main/java/com/javalab/board/Chap65MybatisRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.javalab.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap65MybatisRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap65MybatisRestapiApplication.class, args);
+	}
+
+}
---
+++ chap65_mybatis_restapi/src/main/java/com/javalab/board/vo/User.java
@@ -0,0 +1,20 @@
+package com.javalab.board.vo;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private Integer id;
+	private String password;
+	private String name;
+	private Integer age;
+	private String phone;
+	private String address;
+	private LocalDateTime regDate;
+}
